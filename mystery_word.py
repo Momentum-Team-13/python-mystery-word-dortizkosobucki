e126c1dfcbdd953149832e05ca858d07d8787944
daniela <d.ortizkosobucki@gmail.com>
2022-06-22 14:55:10 -0400

    game works correclty

--- mystery_word.py
+++ mystery_word.py
@@ -9,28 +9,51 @@ def generate_word_for_game():
     answer = word_list [index]
     return answer 
 
-def guess_character():
-    user_guess = input('Guess a letter: ')  
-    if len(user_guess) == 1 and user_guess.isalpha():
-        user_guess = user_guess.lower()
-        # print(f'You guessed {user_guess}')
-        return user_guess  
-    else:    
-        print('Your guess is not a single letter.')   
-        return guess_character()    
-
-def play_game():
+def make_spaces():
     get_word = generate_word_for_game()
+    # print (get_word)
     def split(get_word):
         return list(get_word)
-    split_list = split(get_word)
-    # print(split_list)
-    new_list = [(letter.replace(letter, "_")) for letter in split_list]
-    guesses = []
-    while len(guesses) < 8:
-        guesses.append(guess_character())
-        new_list = [(letter.replace(letter, "_")) if letter not in guesses else letter for letter in split_list] 
-        print(new_list)
+    split_list = list(get_word)
+    spaces = " _ " * len (split_list)
+    print(spaces)
+    return split_list  
+
+def guess_against_word(split_list):
+    correct_letters = []
+    incorrect_letters = []
+    display = [(letter.replace(letter, "_")) if letter not in correct_letters else letter for letter in split_list]
+    # print(display)
+    while len(incorrect_letters) < 8:
+        letter = input("Guess a letter:")
+        if letter.isalpha() and len(letter) == 1:
+            lowercase_guess = letter.lower()
+            if lowercase_guess in split_list:
+                correct_letters.append(lowercase_guess)
+                print (f'Good job, {lowercase_guess} is in the word.')
+                display = [(letter.replace(letter, " _ "))if letter not in correct_letters else letter for letter in split_list]
+                print (display)
+                if display == correct_letters:
+                    print ('You guessed all the letters correctly! Way to go.')
+                    break
+            else: 
+                print(f'{lowercase_guess} is not in the word, try again!')
+                incorrect_letters.append(lowercase_guess)
+        else:
+            print (f"That guess wasn't valid, please try again!")
+        print(f'Letters already guessed:{incorrect_letters + correct_letters}')
+    print ("Game Over!")
+
+def play_game():
+    split_list = make_spaces()
+    start_play = guess_against_word(split_list)
+    # guesses = []
+    # while len(guesses) < 8:
+    #     guesses.append(guess_character())
+    #     new_list = [(letter.replace(letter, "_")) if letter not in guesses else letter for letter in split_list] 
+    #     print(new_list)
 
 if __name__ == "__main__":
     play_game()
+
+    #print (f"the secret wordf conterains {len (random word) } letters")
